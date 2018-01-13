336f65b0d9d4616d3f9eb3b339893d745fd8f687
taylqse04184 <taylqse04184@fpt.edu.vn>
2018-01-13 19:24:48 +0700

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    get "/pages/*page" => "pages#show"
+    root "pages#index", page: "home"
+  end
 end
